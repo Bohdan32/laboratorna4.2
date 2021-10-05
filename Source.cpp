56eff893b8dbbf90580539d5e64fad0da94416cf
bohdan32 <bogdandakun1@gmail.com>
2021-10-05 14:42:28 +0300

    laboratorna 4.2

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= 4) 
+			B = exp(log(2 + 2 * x) + 2 * x);
+		else
+			if (x > 7) 
+				B = 1 - 7 * x + pow(x, 2) - 2 * pow(x, 3); 
+			else
+				B = (1 / tan((1 + x) / 9)) + 8 * x; 
+		y = 1 + x + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
